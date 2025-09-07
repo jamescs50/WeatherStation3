efa2cd98bc7ddac06c5d1d66937af52e3183bedb
James Carr-Saunders <jamescs@btinternet.com>
2025-09-07 11:57:47 +0100

    define pins

--- main/main.c
+++ main/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <inttypes.h>
+#include "hal/gpio_types.h"
 #include "sdkconfig.h"
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
@@ -12,13 +13,6 @@
 #define PIN_READ 25;
 #define PIN_INHIBIT 26;
 
-
-void app_main(void)
-{
-
-}
-
-
 void intToBits(int num, int bits[], int size) {
     for (int i = size - 1; i >= 0; i--) {
         bits[i] = num & 1;   // take the least significant bit
@@ -26,10 +20,38 @@ void intToBits(int num, int bits[], int size) {
     }
 }
 
-int bitsToDecimal(int bits[], int size) {
-    int decimal = 0;
+int bitsToInt(int bits[], int size) {
+    int value = 0;
     for (int i = 0; i < size; i++) {
-        decimal = (decimal << 1) | bits[i];  
+        value = (value << 1) | bits[i];  
     }
-    return decimal;
+    return value;
 }
+
+
+void app_main(void)
+{
+    gpio_set_direction(PIN_A, GPIO_MODE_OUTPUT);
+    gpio_pulldown_en(PIN_A);
+    gpio_pillup_dis(PIN_A);
+    gpio_set_direction(PIN_B, GPIO_MODE_OUTPUT);
+    gpio_pulldown_en(PIN_B);
+    gpio_pillup_dis(PIN_B);
+    gpio_set_direction(PIN_C, GPIO_MODE_OUTPUT);
+    gpio_pulldown_en(PIN_B);
+    gpio_pillup_dis(PIN_C);
+
+
+    int readbits[8];
+    int setbits[3];
+
+    for(int i;i<8;i++){
+        intToBits(i,setbits,3);
+
+
+    }
+
+}
+
+
+
