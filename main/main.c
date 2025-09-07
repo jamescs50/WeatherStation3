ce91d52c2e2030a0e5a7b6cbb4231350b33057cf
James Carr-Saunders <jamescs@btinternet.com>
2025-09-07 11:29:11 +0100

    ReadBits

--- main/main.c
+++ main/main.c
@@ -6,7 +6,30 @@
 #include "freertos/task.h"
 
 
+#define PIN_A 16;
+#define PIN_B 17;
+#define PIN_C 18;
+#define PIN_READ 25;
+#define PIN_INHIBIT 26;
+
+
 void app_main(void)
 {
 
+}
+
+
+void intToBits(int num, int bits[], int size) {
+    for (int i = size - 1; i >= 0; i--) {
+        bits[i] = num & 1;   // take the least significant bit
+        num >>= 1;           // shift right for the next bit
+    }
+}
+
+int bitsToDecimal(int bits[], int size) {
+    int decimal = 0;
+    for (int i = 0; i < size; i++) {
+        decimal = (decimal << 1) | bits[i];  
+    }
+    return decimal;
 }
